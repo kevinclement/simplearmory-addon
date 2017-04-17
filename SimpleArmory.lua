245196325fc103d41dcf07a47f6ac11384684b0d
Kevin Clement <kevin.clement@gmail.com>
2017-04-16 17:41:39 -0700

    add reload command add extended mount information

--- SimpleArmory.lua
+++ SimpleArmory.lua
@@ -2,6 +2,7 @@
 --
 SimpleArmory = {}
 SLASH_SIMPLEARMORY1 = '/sa';
+SLASH_RELOADUI1 = "/rl" -- elvui provides this, but I load only this addon while developing and its nice to have
 
 local toJSON = newencoder(); -- initialize json encoder
 local frame = CreateFrame("Frame")
@@ -15,6 +16,7 @@ end
 local function OnLoad()
     -- hookup slash commands
     SlashCmdList["SIMPLEARMORY"] = OnSlashCommand;
+    SlashCmdList["RELOADUI"] = ReloadUI;
 end
 
 local function OnEvent(self, event, ...)
@@ -42,10 +44,26 @@ function GetAllMounts()
     local mountIDs = C_MountJournal.GetMountIDs();
     for i, id in pairs(mountIDs) do
             local creatureName, spellID, icon, active, isUsable, sourceType, isFavorite, isFactionSpecific, faction, hideOnChar, isCollected, mountID = C_MountJournal.GetMountInfoByID(id)
+            local creatureDisplayID, descriptionText, sourceText, isSelfMount, mountType = C_MountJournal.GetMountInfoExtraByID(id)
             local mountObj = {}
 
             mountObj["name"] = creatureName;
             mountObj["spellID"] = spellID;
+            mountObj["icon"] = icon;
+            mountObj["active"] = active;
+            mountObj["isUsable"] = isUsable;
+            mountObj["sourceType"] = sourceType;
+            mountObj["isFavorite"] = isFavorite;
+            mountObj["isFactionSpecific"] = isFactionSpecific;
+            mountObj["faction"] = faction;
+            mountObj["hideOnChar"] = hideOnChar;
+            mountObj["isCollected"] = isCollected;
+            mountObj["mountID"] = mountID;
+            mountObj["creatureDisplayID"] = creatureDisplayID;
+            mountObj["descriptionText"] = descriptionText;
+            mountObj["sourceText"] = sourceText;
+            mountObj["isSelfMount"] = isSelfMount;
+            mountObj["mountType"] = mountType;
 
             mountList[i] = toJSON(mountObj);
     end --for
