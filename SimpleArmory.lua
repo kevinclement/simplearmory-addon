2b711f307d29ff64ac6fd57905f293831ca57c0e
Antoine Pietri <antoine.pietri1@gmail.com>
2018-12-01 22:00:53 +0100

    Add 'dev' command to export toys/mounts/pets

--- SimpleArmory.lua
+++ SimpleArmory.lua
@@ -25,25 +25,29 @@ function SimpleArmory:ParseCommand(args)
     local command, commandArg1 = self:GetArgs(args, 2)
     if not command then
         SimpleArmory:PrintUsage()
-    else 
+    else
         if command == "toys" then
-            SimpleArmory:ExportToys()
+            SimpleArmory:ShowInFrame(SimpleArmory:ExportToys())
+        elseif command == "dev" then
+            if commandArg1 == "mounts" then
+                SimpleArmory:ShowInFrame(SimpleArmory:GetAllMounts())
+            elseif commandArg1 == "pets" then
+                SimpleArmory:ShowInFrame(SimpleArmory:GetAllPets())
+            elseif commandArg1 == "toys" then
+                SimpleArmory:ShowInFrame(SimpleArmory:GetAllToys())
+            end
         else
             SimpleArmory:PrintUsage()
-        end 
+        end
     end
-    
 end
 
 function SimpleArmory:PrintUsage()
     SimpleArmory:Print("USAGE:")
     SimpleArmory:Print("  /sa toys - exports toys")
-end 
+end
 
-function SimpleArmory:ExportToys()
-    SimpleArmory:Print("Getting all toys from game...")
-    local output = SimpleArmory:GetAllToys()
-
+function SimpleArmory:ShowInFrame(output)
     SACopyFrame:Show()
     SACopyFrameScroll:Show()
     SACopyFrameScrollText:Show()
@@ -67,25 +71,25 @@ function SimpleArmory:GetAllMounts()
             mountObj["name"] = creatureName;
             mountObj["spellID"] = spellID;
             mountObj["icon"] = icon;
-            mountObj["active"] = active;
-            mountObj["isUsable"] = isUsable;
-            mountObj["sourceType"] = sourceType;
-            mountObj["isFavorite"] = isFavorite;
+            --mountObj["active"] = active;
+            --mountObj["isUsable"] = isUsable;
+            --mountObj["sourceType"] = sourceType;
             mountObj["isFactionSpecific"] = isFactionSpecific;
             mountObj["faction"] = faction;
-            mountObj["hideOnChar"] = hideOnChar;
-            mountObj["isCollected"] = isCollected;
+            --mountObj["hideOnChar"] = hideOnChar;
+            --mountObj["isCollected"] = isCollected;
             mountObj["mountID"] = mountID;
-            mountObj["creatureDisplayID"] = creatureDisplayID;
+            --mountObj["creatureDisplayID"] = creatureDisplayID;
             --mountObj["descriptionText"] = descriptionText;
             --mountObj["sourceText"] = sourceText;
-            mountObj["isSelfMount"] = isSelfMount;
-            mountObj["mountType"] = mountType;
+            --mountObj["isSelfMount"] = isSelfMount;
+            --mountObj["mountType"] = mountType;
 
             mountList[i] = toJSON(mountObj);
     end --for
 
     SimpleArmory.MountList = "[" .. table.concat(mountList,",") .. "]";
+    return SimpleArmory.MountList
 end
 
 function SimpleArmory:GetAllPets()
@@ -96,38 +100,55 @@ function SimpleArmory:GetAllPets()
     for i = 1,total do
         --local petID, speciesID, isOwned, _, _, _, _, _, _, _, creatureID = C_PetJournal.GetPetInfoByIndex(i)
         local petID, speciesID, owned, customName, level, favorite, isRevoked, speciesName, icon, petType, companionID, tooltip, description, isWild, canBattle, isTradeable, isUnique, obtainable = C_PetJournal.GetPetInfoByIndex(i);
-        
-        printToChat("petid: " .. petID .. " sid: " .. speciesID .. " create:" .. companionID);
+
+        local petObj = {}
+        petObj["speciesID"] = speciesID;
+        petObj["speciesName"] = speciesName;
+        petObj["companionID"] = companionID;
+        petObj["icon"] = icon;
+        petObj["isWild"] = isWild;
+
+        petList[i] = toJSON(petObj);
     end
-    printToChat("total: " .. total);
+    SimpleArmory.PetList = "[" .. table.concat(petList,",") .. "]";
+    return SimpleArmory.PetList
 end
 
-function SimpleArmory:GetAllToys()
-    output = "[";
+function SimpleArmory:GetToyList()
+    SimpleArmory:Print("Getting all toys from game...")
 
     C_ToyBox.SetAllSourceTypeFilters(true);
     C_ToyBox.SetCollectedShown(true);
     C_ToyBox.SetUncollectedShown(true);
+    C_ToyBox.SetUnusableShown(true);
     C_ToyBox.SetFilterString("");
 
     local NumToys = C_ToyBox.GetNumToys();
-
-    local first = true
-    for i = NumToys, 1, -1 do
-        local idx = C_ToyBox.GetToyFromIndex(i);
-        if PlayerHasToy(idx) then
-            if first == false then
-            output = output .. ", ";
-            else
-            first = false;
-            end
-            output = output .. tostring(idx);
+    local toyList = {};
+    for idx = NumToys, 1, -1 do
+        local itemId = C_ToyBox.GetToyFromIndex(idx)
+        if itemId ~= -1 then
+            table.insert(toyList, itemId)
         end
     end
-    output = output .. "]";
-    return output
+    return toyList
 end
+
+function SimpleArmory:GetAllToys()
+    return toJSON(SimpleArmory:GetToyList());
+end
+
+function SimpleArmory:ExportToys()
+    collectedToyList = {}
+    for i, id in pairs(SimpleArmory:GetToyList()) do
+        if PlayerHasToy(id) then
+            table.insert(collectedToyList, id)
+        end
+    end
+    return toJSON(collectedToyList)
+end
+
 
 function printToChat(msg)
   DEFAULT_CHAT_FRAME:AddMessage(GREEN_FONT_COLOR_CODE.."SA: |r"..tostring(msg))
-end 
+end
